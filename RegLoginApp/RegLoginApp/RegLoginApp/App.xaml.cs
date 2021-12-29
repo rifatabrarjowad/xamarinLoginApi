9931b7cdb87ce011224b66a8264781ce021b8b28
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-12-29 19:18:07 +0600

    update file

---
+++ RegLoginApp/RegLoginApp/RegLoginApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using RegLoginApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RegLoginApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Register();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
