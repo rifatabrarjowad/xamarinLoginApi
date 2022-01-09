6dea3057700e9e7d8c265ed138095744a807eb83
rifatabrarjowad <rajdiprifat2004@gmail.com>
2022-01-09 19:47:11 +0600

    api set

---
+++ okLogin/okLogin/okLogin/Services/ApiServices.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using okLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okLogin.Services
+{
+    public class ApiServices
+    {
+        public async Task<bool> RegisterAsync( string username, string password, string confirmPassword)
+        {
+            var client = new HttpClient();
+            var model = new RegisterBindingModel
+            {
+                UserName = username,
+                Password = password,
+                ConfirmPassword = confirmPassword
+            };
+            var json = JsonConvert.SerializeObject(model);
+            HttpContent content = new StringContent(json);
+            var responce = await client.PostAsync("http://localhost/auth/register.php", content);
+            return responce.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ okLogin/okLogin/okLogin/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,39 @@
+﻿using okLogin.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace okLogin.ViewModel
+{
+    public class RegisterViewModel
+    {
+        ApiServices _apiServices = new ApiServices();
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Massage { get; set; }
+        public ICommand RegisterCommand
+        {
+            get 
+            {
+                return new Command(async () =>
+                {
+                   var isSucess = await _apiServices.RegisterAsync(UserName, Password, ConfirmPassword);
+                    if (isSucess)
+                    {
+                        Massage = "Register sucessfull";
+                    }
+                    else
+                    {
+                        Massage = "try again later";
+                    }
+                        
+                });
+            }
+        }
+
+
+    }
+}
